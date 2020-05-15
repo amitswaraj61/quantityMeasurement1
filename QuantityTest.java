8cfd771307d4cebe3854ad2732bcb62bb7637f41
amitswaraj61 <aswaraj78@gmail.com>
2020-05-15 21:01:55 +0530

    Added Code Given 0 Feet and 1 Feet Should Return Not Equal

--- QuantityTest.java
+++ QuantityTest.java
@@ -7,9 +7,16 @@ import static org.junit.Assert.*;
 public class QuantityTest {
 
     @Test
-    public void given0FeetAnd0Feet_ShouldReturnEqual(){
+    public void given0FeetAnd0Feet_ShouldReturnEqual() {
         Feet feet1 = new Feet(0.0);
         Feet feet2 = new Feet(0.0);
-        assertEquals(feet1,feet2);
+        assertEquals(feet1, feet2);
+    }
+
+    @Test
+    public void given0FeetAnd1Feet_ShouldReturnNotEqual() {
+        Feet feet1 = new Feet(0.0);
+        Feet feet2 = new Feet(1.0);
+        assertNotEquals(feet1, feet2);
     }
 }
